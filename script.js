3de91a31b255c8e6c17b093294adeca4c2ffc5ae
Yousof Khajavi <48098988+Yousoffff@users.noreply.github.com>
2023-09-05 04:22:56 -0800

    working on it

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let datas = {
     hello:"hi",
     mmd:"m",
-    has:"h"
+    has:"h",
+    ff:"ffff"
 }
 let insertData = function(){
     datas[document.getElementById("key").value] = document.getElementById("value").value;
@@ -12,17 +13,57 @@ let insertData = function(){
 //     }
     
 // }
+
+let registerKey = function(){
+    let counter = 0;
+    for(let i in datas){
+        
+        document.getElementsByClassName("card")[counter].innerHTML = i;
+        document.getElementsByClassName("card")[counter].setAttribute("id",i);
+        // document.getElementById("heading").innerHTML = "negar";
+        counter++;
+        }
+}
+registerKey();
+
 let showAnswer = function(){
-for(let i in datas){
-    document.getElementById("card1").innerHTML = datas.hello;
-}
-}
+    document.getElementById(event.srcElement.id).innerHTML = datas[event.srcElement.id];
+    document.getElementById(event.srcElement.id).style =red;
+
+
+    // alert(datas[event.srcElement.id])
+    
+    }
 
     
     let hideAnswer = function(){
-        for(let i in datas){
-        document.getElementById("card1").innerHTML = toString(i);
-    }
+       
+            document.getElementById(event.srcElement.id).innerHTML = event.srcElement.id;
+    
 }
 
+
+
+
+let registerKey1 = function(){
+    // let counter = 0;
+    // for(let i in datas){
+        
+        document.getElementById("testcard").innerHTML = "negar";
+        document.getElementsByClassName("card__face")[counter].setAttribute("id",i);
+        // document.getElementById("heading").innerHTML = "negar";
+        // counter++;
+        // }
+}
+
+
+var cards = document.querySelectorAll('.card');
+
+[...cards].forEach((card)=>{
+  card.addEventListener( 'click', function() {
+    card.classList.toggle('is-flipped');
+  });
+});
+
+registerKey1();
 
